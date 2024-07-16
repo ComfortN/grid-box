606e3bbe76defe98137f41e2be8c504d7ca6374b
Comfort <comfortncp.ngwenya@outlook.com>
2024-07-16 16:03:34 +0200

    responsiveness

--- grid_challenge/src/App.js
+++ grid_challenge/src/App.js
@@ -24,42 +24,42 @@ function App() {
 
       <div className='main'>
         <div className='vees'>
-          <div className='vbox'></div>
-          <div className='vbox'></div>
+          <div className='vbox'>V1</div>
+          <div className='vbox'>v2</div>
         </div>
 
 
         <div className='bees'>
-          <div className='b-box'></div>
-          <div className='b-box'></div>
-          <div className='b-box'></div>
-          <div className='b-box'></div>
+          <div className='b-box'>B1</div>
+          <div className='b-box'>B2</div>
+          <div className='b-box'>B3</div>
+          <div className='b-box'>B4</div>
         </div>
 
 
         <div className='lboxes'>
-          <div className='l-box'></div>
-          <div className='l-box'></div>
-          <div className='l-box'></div>
-          <div className='l-box'></div>
+          <div className='l-box'>L1</div>
+          <div className='l-box'>L2</div>
+          <div className='l-box'>L3</div>
+          <div className='l-box'>L4</div>
         </div>
 
 
         <div className='row-box'>
-        <div className='row-b'></div>
+        <div className='row-b'>ROW</div>
         </div>
       </div>
 
       <div className='footer'>
-        <div className='footer-box'></div>
-        <div className='footer-box'></div>
-        <div className='footer-box'></div>
-        <div className='footer-box'></div>
-        <div className='footer-box'></div>
-        <div className='footer-box'></div>
-        <div className='footer-box'></div>
-        <div className='footer-box'></div>
-        <div className='footer-box'></div>
+        <div className='footer-box'>1</div>
+        <div className='footer-box'>2</div>
+        <div className='footer-box'>3</div>
+        <div className='footer-box'>4</div>
+        <div className='footer-box'>5</div>
+        <div className='footer-box'>6</div>
+        <div className='footer-box'>7</div>
+        <div className='footer-box'>8</div>
+        <div className='footer-box'>9</div>
 
       </div>
 
