6e32109628607b2af19fefd28bcbd5a6b45bb51b
Comfort Ngwenya <>
2024-07-16 03:06:30 +0200

    grid

---
+++ grid_challenge/src/App.js
@@ -0,0 +1,70 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='navbar'>
+        <div className='logo'>
+          <div className='logos'></div>
+        </div>
+
+        <div className='navlist'>
+          <div className='lists'>Home</div>
+          <div className='lists'>About</div>
+          <div className='lists'>Contact</div>
+          <div className='lists'>Gallery</div>
+          <div className='lists'>Post</div>
+        </div>
+
+        <div className='navbtn'>
+          <div className='btn'>BTN</div>
+        </div>
+      </div>
+
+      <div className='main'>
+        <div className='vees'>
+          <div className='vbox'></div>
+          <div className='vbox'></div>
+        </div>
+
+
+        <div className='bees'>
+          <div className='b-box'></div>
+          <div className='b-box'></div>
+          <div className='b-box'></div>
+          <div className='b-box'></div>
+        </div>
+
+
+        <div className='lboxes'>
+          <div className='l-box'></div>
+          <div className='l-box'></div>
+          <div className='l-box'></div>
+          <div className='l-box'></div>
+        </div>
+
+
+        <div className='row-box'>
+        <div className='row-b'></div>
+        </div>
+      </div>
+
+      <div className='footer'>
+        <div className='footer-box'></div>
+        <div className='footer-box'></div>
+        <div className='footer-box'></div>
+        <div className='footer-box'></div>
+        <div className='footer-box'></div>
+        <div className='footer-box'></div>
+        <div className='footer-box'></div>
+        <div className='footer-box'></div>
+        <div className='footer-box'></div>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
